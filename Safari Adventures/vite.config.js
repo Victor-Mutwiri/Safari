6699c1a1ba2210a734386373499025c04ad356fa
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2023-12-18 20:40:02 +0300

    1.91

--- Safari Adventures/vite.config.js
+++ Safari Adventures/vite.config.js
@@ -8,10 +8,10 @@ export default defineConfig({
     fs: {
       // Include your project source directory
       allow: [
-        'C:/Users/dell/Desktop/Safari Adventures/Safari Adventures/src',
-        'C:/Users/dell/Desktop/Safari Adventures/Safari Adventures/node_modules',
+        'C:./src',
+        'C:./node_modules',
         'C:/Users/dell/Desktop/Safari Adventures/Safari Adventures/node_modules/slick-carousel/slick/**',
-        'C:/Users/dell/Desktop/Safari Adventures/node_modules/vite/dist/client/**',
+        './node_modules/vite/dist/client/**',
         'C:/Users/dell/Desktop/Safari Adventures/node_modules/slick-carousel/slick/fonts/slick.woff',
         'C:/Users/dell/Desktop/Safari Adventures/node_modules/slick-carousel/slick/fonts/slick.ttf',
       ],
